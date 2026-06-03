a17a685b9e6816b32ca045f8a730d2b0b30d42fa
241230061-debug <241230061@unmuhpnk.ac.id>
2026-06-03 12:18:35 +0700

    update ada view login

---
+++ viewlogin.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple PHP test page.</p>
+    <?php
+        // This is a PHP code block
+        echo "<p>Current date and time: " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+    
+</html>
